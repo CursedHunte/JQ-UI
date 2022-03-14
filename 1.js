52ed946a6b127676ef18a2e96b81b4f1b152ca70
CursedHunte <77138824+CursedHunte@users.noreply.github.com>
2022-03-14 12:48:54 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+   
+   $("#el1").draggable({revert:true}).resizable();
+   $("#sort1").sortable();
+   $("#el2,#el3").draggable({revent:true});
+   $("#drop1").droppable({
+	   accept:'#el2,#el3',
+	   drop:function(event,ui){ui.draggable.css("display","none");
+	   $("#drop1").css("background-color","green");}});
+	   
+	   $("#drop2").droppable({accept:"#el3", drop:function(){
+	   $("#el3").css("display","none");
+	   $("#drop2").css("background-color","green");}});
+      });
